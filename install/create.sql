4b91e342ec739c972e65120aa18d1bc57606eb85
Mark Farrell <markfarr2011@gmail.com>
2014-02-20 11:14:02 -0500

    -- Added schema for storing terms in a PostgreSQL database.
    Signed off by: Mark Farrell<markfarr2011@gmail.com>

--- install/create.sql
+++ install/create.sql
@@ -1,7 +1,46 @@
 /** 
  * @name create
  * @author Mark Farrell
- * @description requires the database for this project.
+ * @description requires the database and its tables for this project.
  **/
 
-CREATE DATABASE IF NOT EXISTS berkeley;
+CREATE DATABASE IF NOT EXISTS beagle;
+
+-- TODO: 
+-- OR conditions for actions: multiple entries, same value
+-- AND conditions: conditions point to conditions
+
+CREATE TABLE IF NOT EXISTS topics (
+    id integer PRIMARY KEY,
+    value text, 
+    dependency_id integer,
+    FOREIGN KEY (dependency_id) REFERENCES dependencies (id) ON DELETE CASCADE 
+)
+
+
+CREATE TABLE IF NOT EXISTS actions (
+    id integer PRIMARY KEY,
+    value text, 
+    condition_id integer,
+    topic_id integer,
+    FOREIGN KEY (condition_id) REFERENCES conditions (id) ON DELETE CASCADE
+    FOREIGN KEY (topic_id) REFERENCES topics (id) ON DELETE CASCADE
+    CONSTRAINT CHECK ((condition_id IS NOT NULL AND topic_id IS NULL) OR (condition_id IS NULL AND topic_id IS NOT NULL)) -- XOR constraint 
+)
+
+CREATE TABLE IF NOT EXISTS conditions ( 
+    id integer PRIMARY KEY,
+    value text,
+    topic_id integer,
+    FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE
+    CONSTRAINT CHECK (topic_id IS NOT NULL)
+)
+
+CREATE TABLE IF NOT EXISTS dependencies ( 
+    id integer PRIMARY KEY,
+    value text,
+    action_id integer, 
+    FOREIGN KEY (action_id) REFERENCES actions (id) ON DELETE CASCADE
+    CONSTRAINT CHECK (action_id IS NOT NULL)
+) 
+ 
