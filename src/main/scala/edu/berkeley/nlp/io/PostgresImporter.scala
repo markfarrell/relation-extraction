7c170d44b85d98564b11bc31f9c9f9e30fa0df57
Mark Farrell <markfarr2011@gmail.com>
2014-03-04 18:40:47 -0500

    -- Continued working on PostgresImporter, allowing metadata to be collected about an environment so that it can be constructed.
    Signed off by: Mark Farrell<markfarr2011@gmail.com>

--- src/main/scala/edu/berkeley/nlp/io/PostgresImporter.scala
+++ src/main/scala/edu/berkeley/nlp/io/PostgresImporter.scala
@@ -25,12 +25,13 @@ import edu.berkeley.nlp.syntax.Environment.Action
  **/
 class PostgreImporter(conn : Connection) { 
 
-  private class ConditionMetadata extends HashMap[Int, String]
-  private class ActionMetadata extends HashMap[Int, String]
-  private class DependencyMetadata extends HashMap[Int, String]
+  private class Metadata extends HashMap[Int, String]
+  private class ConditionMetadata extends Metadata
+  private class ActionMetadata extends Metadata
+  private case class DependencyMetadata(id : Int, value : String, actionId : Int)
   private case class TopicMetadata(value : String, conditionMetadata : ConditionMetadata, actionMetadata : ActionMetadata) 
 
-  private class DependencyGraph extends HashMap[ActionMetadata, (DependencyMetadata, TopicMetadata)] 
+  private class DependencyGraph extends HashMap[DependencyMetadata, String] 
 
   private def selectTopics() : PreparedStatement = conn.prepareStatement("SELECT * FROM topics")
   private def selectAbsoluteActions() : PreparedStatement  = conn.prepareStatement("SELECT * FROM actions WHERE topic_id = ?")
@@ -57,6 +58,15 @@ class PostgreImporter(conn : Connection) {
     } 
 
     topicValues 
+  }
+
+  private def fetchMetadata[T <: Metadata](ps : PreparedStatement, map : T) : T = { 
+    val rs : ResultSet = ps.executeQuery()
+    while(rs.next()) { 
+      map += (rs.getInt(1) -> rs.getString(2))
+    } 
+    ps.close()
+    map
   } 
 
   /**
@@ -64,50 +74,129 @@ class PostgreImporter(conn : Connection) {
     * @param topicId 
     * @return 
    **/
-  private def getAbsoluteActionMetadata(topicId : String) : ActionMetadata = { 
+  private def getAbsoluteActionMetadata(topicId : String) : ActionMetadata = {
+
     val selectedAbsoluteActions : PreparedStatement = selectAbsoluteActions()
     selectedAbsoluteActions.setString(1, topicId)
     
-    {
-      var map : ActionMetadata = new ActionMetadata
-      val rs : ResultSet = selectedAbsoluteActions.executeQuery()
-      while(rs.next()) { 
-        map += (rs.getInt(1) -> rs.getString(2)) 
-      } 
-      selectedAbsoluteActions.close()
-      map
-    } 
+    fetchMetadata[ActionMetadata](selectedAbsoluteActions, new ActionMetadata) 
     
   }
 
-  private def getConditionalMetadata(conditionId : Int) : ActionMetadata = { 
+  private def getConditionalActionMetadata(conditionId : Int) : ActionMetadata = { 
+
     val selectedConditionalActions : PreparedStatement = selectConditionalActions()
     selectedConditionalActions.setInt(1, conditionId)
 
-    { 
-      var map : ActionMetadata = new ActionMetadata
-      val rs : ResultSet = selectedConditionalActions.executeQuery()
-      while(rs.next()) { 
-        map += (rs.getInt(1) -> rs.getString(2))
-      } 
-      selectedConditionalActions.close()
-      map
-    } 
+    fetchMetadata[ActionMetadata](selectedConditionalActions, new ActionMetadata) 
 
   } 
 
+  private def getActionMetadata(topicMetadata : TopicMetadata) : ActionMetadata = {
+
+    var actionMetadata : ActionMetadata = topicMetadata.actionMetadata
+
+    // Extract all actions that point to the conditions that points to the topic
+    val ls : List[(Int, String)] = {
+      topicMetadata.conditionMetadata.keys flatMap { 
+        getConditionalActionMetadata(_)
+      } toList
+    } 
+
+    // Iterate and add each entry (keeps the type of ActionMetadata)
+    for(l <- ls) { 
+      actionMetadata += l
+    } 
+
+    actionMetadata 
+  } 
+
+  private def getConditionMetadata(topicId : String) : ConditionMetadata = { 
+
+    val selectedConditions : PreparedStatement = selectConditions()
+    selectedConditions.setString(1, topicId) 
+
+    fetchMetadata[ConditionMetadata](selectedConditions, new ConditionMetadata) 
+
+  }
+
+  private def getDependencyGraph(actionId : Int, topicId : String) : DependencyGraph = { 
+    
+    val selectedDependencies : PreparedStatement = selectDependencies() 
+    selectedDependencies.setInt(1, actionId)
+
+    {   
+
+      val graph : DependencyGraph = new DependencyGraph
+      val ps : PreparedStatement = selectDependencies()
+      val rs : ResultSet = ps.executeQuery()
+
+      while(rs.next()) {
+
+        val dependencyMetadata : DependencyMetadata = DependencyMetadata(rs.getInt(1), rs.getString(2), rs.getInt(3)) 
+        val topicId : String = rs.getString(4)
+
+        graph += dependencyMetadata -> topicId
+
+      } 
+
+      ps.close()
+      graph
+
+    }
+
+  }
+
+  private def getTopicMetadata(topicId : String) : TopicMetadata = { 
+
+    val actionMetadata : ActionMetadata = getAbsoluteActionMetadata(topicId)
+    val conditionMetadata : ConditionMetadata = getConditionMetadata(topicId) 
+    
+    TopicMetadata(topicId, conditionMetadata, actionMetadata) 
+    
+  }
+
+ 
   def load() : Environment = {
 
     val env : Environment = new Environment
 
-    // TODO:
-    // Finish metadata methods 
-    //
-    // Construct all TopicMetadata
-    // Create empty DependencyGraph
-    // For each TopicMetadata, take its ActionMetadata and select dependencies for each action id
-    // Insert each (DependencyMetadata, TopicMetadata) result into the table IF it does not already exist (to avoid cycles) 
+    val topicList : List[String] = getTopicList
 
+    val topicsMetadata : List[TopicMetadata] = topicList map { 
+      getTopicMetadata(_)
+    }
+
+    var topics : List[Topic] = List.empty[Topic]
+
+    { 
+
+      for(topicMetadata <- topicsMetadata) {
+
+        val topic : Topic = Topic(topicMetadata.value, List.empty[Term])
+
+        val actionMetadata : ActionMetadata = getActionMetadata(topicMetadata) 
+        // Get rid of getActionMetadata; 
+        // Create Action, or Condition that points to Actions 
+
+        for { 
+          actionId <- actionMetadata.keys
+          tuple <- getDependencyGraph(actionId, topicMetadata.value)
+        } yield Dependency(tuple._1.value, List(Topic(tuple._2, List.empty[Term])))
+        // How will dependencies merge together? 
+
+        // Create dependency that points to its Topic (with no terms) 
+        // Correct topic will be reconstructed when inserted into the environment
+
+        topics = topic :: topics
+
+      }
+
+    } 
+
+    env.insertTopics(topics)
+
+ 
     env 
   } 
 
