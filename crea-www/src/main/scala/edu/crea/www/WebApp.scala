2289ff42c22f9236dacd1b30be953a3834b78174
m4farrel <m4farrel@csclub.uwaterloo.ca>
2014-08-02 10:56:07 -0300

    -- Added auto-complete functionality to the search bar.
    Signed-off-by: Mark Farrell<m4farrel@csclub.uwaterloo.ca>

--- crea-www/src/main/scala/edu/crea/www/WebApp.scala
+++ crea-www/src/main/scala/edu/crea/www/WebApp.scala
@@ -1,5 +1,8 @@
 package edu.crea.www
 
+import scala.util.matching.Regex
+import scala.util.{Try, Success, Failure}
+
 import scala.scalajs._
 import scala.scalajs.js.Dynamic.newInstance
 import js.annotation.JSExport
@@ -9,11 +12,14 @@ import dom.document
 
 object WebApp extends js.JSApp {
 
-  private[this] def gexfPath = "data/elastin-abstracts.gexf"
+  private[this] def gexfPath : String = "data/elastin-abstracts.gexf"
 
-  private[this] def searchInput : Option[dom.HTMLElement] = { 
+
+  private[this] def autocomplete(input : String) : Regex = input.toList.mkString("""[\s\w]*""").concat("""[\s\w]*""").r
+
+  private[this] def searchInput : Option[dom.HTMLElement] = {
     Option(document.getElementById("search-input"))
-  } 
+  }
 
   private[this] def sigmajs = js.Dynamic.global.sigma
 
@@ -46,21 +52,42 @@ object WebApp extends js.JSApp {
 
   private[this] lazy val sigma = newInstance(sigmajs)(config)
 
-  private[this] var maybeGraph : Option[js.Dynamic] = None
+  private[this] def dictionary : Array[String] = {
+    sigma.graph.nodes().asInstanceOf[js.Array[js.Dynamic]].map((_ : js.Dynamic).label.asInstanceOf[String])
+  }
 
-  private[this] def resetGraph() : Unit = maybeGraph.map { graph => 
+  private[this] var findGraph : Option[js.Dynamic] = None
+
+  private[this] def resetGraph() : Unit = findGraph.map { graph =>
 
     sigma.graph.clear()
     sigma.graph.read(js.Dynamic.literal(nodes = graph.nodes(), edges = graph.edges()))
-    sigma.refresh() 
+    sigma.refresh()
 
-  } 
+  }
 
-  private[this] def viewNeighborhood(id : js.Dynamic) = maybeGraph.map { graph =>
+  private[this] def findNeighborhood(id : js.Dynamic) : Option[js.Dynamic] = findGraph.flatMap { graph =>
 
-    val neighborhood = graph.neighborhood(id)
+    Try(graph.neighborhood(id)) match {
 
-    if(neighborhood.edges.length > 1) {
+      case Success(neighborhood) =>
+
+        if(neighborhood.nodes.length > 1) {
+          Some(neighborhood)
+        } else {
+          None
+        }
+
+      case Failure(_) => None
+
+    }
+
+  }
+
+
+  private[this] def viewNeighborhood(id : js.Dynamic) = findGraph.map { graph =>
+
+    for(neighborhood <- findNeighborhood(id)) {
 
       sigma.graph.clear()
       sigma.graph.read(neighborhood)
@@ -70,32 +97,81 @@ object WebApp extends js.JSApp {
         angle = 0,
         ratio = 1
       ))
+
       sigma.refresh()
+
+     }
+
+  }
+
+  private[this] def findLabel : Option[String] = searchInput.map(_.asInstanceOf[js.Dynamic].value.asInstanceOf[String])
+
+  private[this] def findId(label : String) : Option[js.Dynamic] = findGraph.flatMap { graph => Option {
+    graph.nodes().asInstanceOf[js.Array[js.Dynamic]]
+      .filter((elem : js.Dynamic) => elem.label.asInstanceOf[String] == label)
+      .map((elem : js.Dynamic) => elem.id)
+      .pop
+  }}
+
+  @JSExport
+  def search() = for {
+    label <- findLabel
+    id <- findId(label)
+  } {
+    viewNeighborhood(id)
+  }
+
+  @JSExport
+  def showSuggestions() = Option(document.getElementById("suggestions")).map { suggestions =>
+
+    while(suggestions.hasChildNodes()) {
+      suggestions.removeChild(suggestions.lastChild)
+    }
+
+    for {
+      label <- findLabel
+    } {
+
+      val dict = dictionary
+
+      if(dict.size <= 500) {
+
+        val items = dictionary.filter(suggestion => autocomplete(label).pattern.matcher(suggestion).matches)
+          .sortBy(Levenshtein(label))
+          .take(20)
+
+        if(items.length > 0) {
+
+          items.foreach { suggestion =>
+
+            val li = document.createElement("li")
+            li.onclick = ( e : dom.MouseEvent) => findId(suggestion).foreach(id => viewNeighborhood(id))
+            li.appendChild(document.createTextNode(suggestion))
+
+            suggestions.appendChild(li)
+
+          }
+
+        suggestions.style.display = "block";
+
+        } else {
+
+          hideSuggestions();
+
+        }
+
+      }
 
     }
 
-  } 
+  }
 
-  @JSExport 
-  def search() = { 
-    
-    def findLabel = searchInput.map(_.asInstanceOf[js.Dynamic].value)
+  @JSExport
+  def hideSuggestions() = {
 
-    def findId(label : js.Dynamic) : Option[js.Dynamic] = maybeGraph.flatMap { graph => Option { 
-      graph.nodes().asInstanceOf[js.Array[js.Dynamic]]
-        .filter((elem : js.Dynamic) => elem.label == label)
-        .map((elem : js.Dynamic) => elem.id)
-        .pop
-    }}
+    document.getElementById("suggestions").style.display = "none";
 
-    for { 
-      label <- findLabel
-      id <- findId(label)
-    } { 
-      viewNeighborhood(id)
-    } 
-
-  } 
+  }
 
   def main() = {
 
@@ -103,7 +179,7 @@ object WebApp extends js.JSApp {
 
     sigmajs.parsers.gexf(gexfPath, js.Dynamic.literal(), { (gexfSig : js.Dynamic) =>
 
-      maybeGraph = Option(gexfSig.graph)
+      findGraph = Option(gexfSig.graph)
 
       resetGraph()
 
